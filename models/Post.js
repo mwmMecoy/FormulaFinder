87643564c654a2d1bee95c75b0ae738466a1df26
mwmMecoy <macmecoy@gmail.com>
2022-09-20 19:47:02 -0500

    Started setting up Location models

--- models/Post.js
+++ models/Post.js
@@ -1,29 +1,13 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  title: {
+  company: {
     type: String,
     required: true,
   },
-  image: {
-    type: String,
-    require: true,
-  },
-  cloudinaryId: {
-    type: String,
-    require: true,
-  },
-  caption: {
+  address: {
     type: String,
     required: true,
-  },
-  likes: {
-    type: Number,
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
   },
   createdAt: {
     type: Date,
